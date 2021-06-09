12bae17f24531fbe91ac0632e5489b0c47951df9
Artem Onufrei <2hilderoy2@gmail.com>
2021-06-09 15:31:15 +0300

    Added validations for microposts and set relationship between user and micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,10 @@
 class Micropost < ApplicationRecord
+    belongs_to :user
+
+    validates :content, length: {
+        minimum: 3,
+        maximum: 300
+    }
+    
+    validates :user_id, presence: true
 end
